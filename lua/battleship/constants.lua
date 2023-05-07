2d72aeaa77fb4feda6b3556b63518cb400956e6c
Victor Oliveira <eu@victoroliveira.com.br>
2023-05-07 13:01:10 -0300

    fix: fixes typing of board default opts in constants

--- lua/battleship/constants.lua
+++ lua/battleship/constants.lua
@@ -1,29 +1,23 @@
----@class UiDefaultOpts
----@field height number
----@field width number
----@field row number
----@field col number
-
 return {
     BATTLESHIP_AUTO_GROUP = "BattleshipAuGroup",
     ---@type string[]
     BOARD_ROWS = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" },
     UI = {
-        ---@type UiDefaultOpts
+        ---@type InterfaceOptions
         BOARD_DEFAULT_OPTS = {
             height = 20,
             width = 45,
             row = 5,
             col = 10,
         },
-        ---@type UiDefaultOpts
+        ---@type InterfaceOptions
         PROMPT_DEFAULT_OPTS = {
             height = 1,
             width = 45,
             row = 27,
             col = 10,
         },
-        ---@type UiDefaultOpts
+        ---@type InterfaceOptions
         LOG_DEFAULT_OPTS = {
             height = 23,
             width = 30,
