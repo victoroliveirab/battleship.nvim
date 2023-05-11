ec7b3fef78e01cd07143e42b2652f3f47b97cd18
Victor Oliveira <eu@victoroliveira.com.br>
2023-05-11 19:47:49 -0300

    feat: makes player input reading more robust

--- lua/battleship/ui/prompt.lua
+++ lua/battleship/ui/prompt.lua
@@ -14,6 +14,18 @@ local read_buffer = function(buffer)
     return vim.api.nvim_buf_get_lines(buffer, 0, 1, false)[1]
 end
 
+---Parses the input
+---@param input string Player's input
+---@return Coordinates
+local parse_input = function(input)
+    local row = input:sub(1, 1)
+    local col = tonumber(input:sub(2, 2))
+    if not col or #input ~= 2 then
+        error("Wrong format")
+    end
+    return { row = row, col = col }
+end
+
 ---Creates a new prompt interface
 ---@param opts InterfaceOptions
 ---@return Interface
@@ -31,12 +43,13 @@ function PromptInterface:read(on_submit)
     vim.api.nvim_feedkeys("i", "n", true)
     vim.keymap.set("i", "<CR>", function()
         local content = read_buffer(self.buf)
-        vim.cmd("stopinsert")
         vim.api.nvim_buf_set_lines(self.buf, 0, 1, false, { "" })
-        local row = content:sub(1, 1)
-        local col = tonumber(content:sub(2, 2))
-        vim.keymap.del("i", "<CR>", { buffer = self.buf })
-        on_submit({ row = row, col = col })
+        local ok, coordinates = pcall(parse_input, content)
+        if ok then
+            vim.cmd("stopinsert")
+            vim.keymap.del("i", "<CR>", { buffer = self.buf })
+            on_submit(coordinates)
+        end
     end, { buffer = self.buf, silent = true })
 end
 
