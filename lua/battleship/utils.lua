10745f680df0ac999020168b80a11cf6c6d306b9
Victor Oliveira <eu@victoroliveira.com.br>
2023-05-16 14:01:52 -0300

    feat: adds several utility functions

--- lua/battleship/utils.lua
+++ lua/battleship/utils.lua
@@ -18,6 +18,54 @@ local function deepcopy(orig)
     return copy
 end
 
+---Filters an array
+---@generic T
+---@param arr T[]
+---@param predicate fun(element: T): boolean
+local function filter(arr, predicate)
+    local filtered_arr = {}
+    for _, element in ipairs(arr) do
+        if predicate(element) then
+            table.insert(filtered_arr, element)
+        end
+    end
+    return filtered_arr
+end
+
+---Checks if an element belongs to an array
+---@generic T
+---@param arr T[]
+---@param element T
+---@param equality_fn fun(a: T, b: T)?: boolean
+---@return boolean
+local function includes(arr, element, equality_fn)
+    equality_fn = equality_fn or function(a, b)
+        return a == b
+    end
+    for _, value in pairs(arr) do
+        if equality_fn(element, value) then
+            return true
+        end
+    end
+    return false
+end
+
+---Creates an equal padding on both sizes of the string
+---@param str string the uncentered string
+---@param length integer the final length of the string
+---@param char string? the string to use on padding
+---@return string str str centralized
+local function centralize_string(str, length, char)
+    char = char or " "
+    local available_length = length - #str
+    local left_pad = math.floor(available_length / 2)
+    local right_pad = math.ceil(available_length / 2)
+    return string.format("%s%s%s", char:rep(left_pad), str, char:rep(right_pad))
+end
+
 return {
+    centralize_string = centralize_string,
     deepcopy = deepcopy,
+    filter = filter,
+    includes = includes,
 }
