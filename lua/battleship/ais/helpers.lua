bab04926e0be52282251462cc731e0725da06532
Victor Oliveira <eu@victoroliveira.com.br>
2023-05-20 12:54:24 -0300

    feat: updates ai helpers to use Point

--- lua/battleship/ais/helpers.lua
+++ lua/battleship/ais/helpers.lua
@@ -1,27 +1,11 @@
+local Point = require("battleship.boards.point")
+
 local constants = require("battleship.constants")
 local utils = require("battleship.utils")
 
 local ROWS = constants.BOARD_ROWS
 
 -- TODO: transfer this functions to a more generic board helpers file
-
----Get row index
----@param row string
----@return integer? row Row's index
-local get_row_index = function(row)
-    for index, value in ipairs(constants.BOARD_ROWS) do
-        if value == row then
-            return index
-        end
-    end
-end
-
----Checks if a point is valid
----@param coordinate Coordinates
----@return boolean? is_valid Whether coordinate is valid
-local is_valid_point = function(coordinate)
-    return get_row_index(coordinate.row) and coordinate.col >= 0 and coordinate.col < 10
-end
 
 ---Gets a random point on the board
 ---@return Coordinates
@@ -35,18 +19,18 @@ local get_random_point = function()
 end
 
 ---Gets the points surrouding a point in the board.
----@param point Coordinates
----@return Coordinates[] points The at most 4 points that surround the given point
+---@param point Point
+---@return Point[] points The at most 4 points that surround the given point
 local get_surrounding_points = function(point)
-    local row_index = get_row_index(point.row)
-    local col_index = point.col
+    local row_index = point.row_index
+    local col = point.col
 
-    local left_point = { row = ROWS[row_index], col = col_index - 1 }
-    local right_point = { row = ROWS[row_index], col = col_index + 1 }
-    local up_point = { row = ROWS[row_index - 1], col = col_index }
-    local down_point = { row = ROWS[row_index + 1], col = col_index }
+    local left_point = Point.create({ row = ROWS[row_index], col = col - 1 })
+    local right_point = Point.create({ row = ROWS[row_index], col = col + 1 })
+    local up_point = Point.create({ row = ROWS[row_index - 1], col = col })
+    local down_point = Point.create({ row = ROWS[row_index + 1], col = col })
 
-    return utils.filter({ left_point, right_point, up_point, down_point }, is_valid_point)
+    return utils.filter({ left_point, right_point, up_point, down_point }, Point.is_valid)
 end
 
 return {
