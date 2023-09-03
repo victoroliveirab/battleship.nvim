3840ad68e8f5fda711d52c746ee337de6d6361d0
Victor Oliveira <eu@victoroliveira.com.br>
2023-09-03 10:35:37 -0300

    feat: adds argument completion for Battleship command

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,9 +1,49 @@
 local Game = require("battleship.loop")
 local Stats = require("battleship.stats")
 
-vim.api.nvim_create_user_command("Battleship", function()
-    require("battleship").setup({ run_tests = true })
-    local game = Game:new()
+local available_args = {
+    "easy",
+    "easy_quick",
+    "medium",
+    "medium_quick",
+}
+
+---@param difficulty string?
+local parse_difficulty = function(difficulty)
+    if not difficulty or difficulty == "medium" then
+        return "medium"
+    end
+    if difficulty == "easy" then
+        return "easy"
+    end
+    return "medium"
+end
+
+vim.api.nvim_create_user_command("Battleship", function(command)
+    if not vim.g.battleship_setup then
+        require("battleship").setup({ run_tests = true })
+    end
     Stats.init()
+    local difficulty, mode = unpack(vim.split(command.args, "_"))
+    -- Ready for assemble board code
+    if not mode or mode ~= "quick" then
+        difficulty = parse_difficulty(difficulty)
+        local game = Game:new({
+            difficulty = difficulty,
+        })
+        game:start()
+        return
+    end
+    local game = Game:new({
+        difficulty = difficulty,
+    })
     game:start()
-end, { desc = "Start a new game of Battleship with default settings" })
+end, {
+    desc = "Start a new game of Battleship with default settings",
+    nargs = "?",
+    complete = function(arg)
+        return vim.tbl_filter(function(val)
+            return vim.startswith(val, arg)
+        end, available_args)
+    end,
+})
